f02851c49d1abd9386b0eb28c2a58076bffa7b24
Omar.nadif <Omar.nadif2022@campus-eni.fr>
2024-02-23 16:44:07 +0100

    Commit Omar - Add EARTH

--- lib/ui/screen/home_page.dart
+++ lib/ui/screen/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
 import 'package:suivi_iss/ui/screen/maps_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,15 +11,51 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => MapsPage()),
-            );
-          },
-          child: Text('Find Space Station '),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/starsBackground.webp"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Expanded(
+              child: ModelViewer(
+                src: 'assets/models/Earth.glb',
+                iosSrc: 'assets/models/Earth.glb',
+                arModes: ['scene-viewer', 'quick-look'],
+                alt: "Earth",
+                ar: false,
+                autoRotate: true,
+                disableZoom: true,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MapsPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white, backgroundColor: Colors.transparent,
+                  side: const BorderSide(color: Colors.white, width: 2),
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: const Text(
+                  'Find Space Station',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
